7aab55ba6515b7188c880afd114c6960ed6468f1
dsm5e <justry24@gmail.com>
2023-08-03 13:21:30 +0300

    Initial Commit

---
+++ WeatherWizard/WeatherWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWizardApp.swift
+//  WeatherWizard
+//
+//  Created by dsm 5e on 03.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
